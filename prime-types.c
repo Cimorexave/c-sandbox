cc44914980cb2ff22ba8d8f9cfb11b15b257a2ad
Cimorexave <sadeq.s502@gmail.com>
2023-07-31 21:44:16 +0330

    experimenting with prime types

--- prime-types.c
+++ prime-types.c
@@ -10,10 +10,25 @@ void do_nothing()
     printf("\n not doing anything... \n");
 }
 
+float add2_f(float one, float two)
+{
+    return one + two;
+}
+
+double add2_d(float one, float two)
+{
+    return one + two;
+}
+
 void main()
 {
     printf("starting... \n");
-    printf("the sum of 2 and 5 is %i", add2(2, 5));
-    do_nothing();
-    printf("the sum of 2 and 5 is %i", add2(2, 5));
+
+    // printf("the sum of 2 and 5 is %i \n", add2(2, 5));
+
+    // do_nothing();
+
+    // printf("the sum of 2 and 5 is %i \n", add2_f(2.0, 5.0));
+
+    // printf("the sum of 2 and 5 is %i \n", add2_d(2, 5));
 }
