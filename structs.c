598658d0e442ffbe11eae5aadb747c7df22aa3c6
Cimorexave <sadeq.s502@gmail.com>
2023-07-31 21:44:22 +0330

    structs exp.

---
+++ structs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void say_something()
+{
+    printf("hi");
+}
+
+struct person
+{
+    unsigned short age;
+    unsigned int id;
+};
+
+void main()
+{
+
+    say_something();
+
+    // person.age me.age = 10;
+}
