bd579a7aa20bcfed0f4e4e05d7a7ca6ffe402057
Cimorexave <sadeq.s502@gmail.com>
2023-08-01 00:59:32 +0330

    hob game and find mods func

--- decay.c
+++ decay.c
@@ -21,11 +21,58 @@ void print_times(int times)
     }
 }
 
+void say_hob(int hob)
+{
+    int i = 0;
+    int condition = 1;
+    while (condition)
+    {
+        if (i % hob == 0)
+            printf("hob\n");
+        else
+            printf("%i\n", i);
+        if (i == 100)
+            condition = 0;
+        i++;
+    }
+}
+
+void find_mods(int mod)
+{
+    // int arr[];
+    int size_of_mods_arr = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % mod == 0)
+            size_of_mods_arr++;
+        // arr[i] = i;
+    }
+
+    int arr[size_of_mods_arr];
+    int counter = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % mod == 0)
+            arr[++counter] = i;
+        // size_of_mods_arr++;
+    }
+
+    for (int i = 0; i < size_of_mods_arr; i++)
+    {
+        printf("arr[%i]: %i\n", i, arr[i]);
+    }
+}
+
 int main()
 {
     // decay_func(10);
 
     // print_times(30);
+
+    // say_hob(5);
+    // say_hob(7);
+
+    find_mods(5);
 
     return 0;
 }
