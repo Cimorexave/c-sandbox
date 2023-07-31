2577c9126cad153b3b5b7bf862eb10fc6f6096e5
Cimorexave <sadeq.s502@gmail.com>
2023-08-01 00:42:14 +0330

    pyramid decay

--- decay.c
+++ decay.c
@@ -7,9 +7,25 @@ void decay_func(int top)
         decay_func(--top);
 }
 
+void print_times(int times)
+{
+
+    if (times > 0)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        print_times(--times);
+    }
+}
+
 int main()
 {
-    decay_func(10);
+    // decay_func(10);
+
+    // print_times(30);
 
     return 0;
 }
