f1f780276038e7a785b5f396fe748120027193f6
Cimorexave <sadeq.s502@gmail.com>
2023-08-03 00:09:28 +0330

    seeing the assembly code how allocates memory

---
+++ asm_alloc.s
@@ -0,0 +1,23 @@
+	.file	"asm_alloc.c"
+	.text
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	call	___main
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.ident	"GCC: (i686-win32-dwarf-rev0, Built by MinGW-W64 project) 7.3.0"
