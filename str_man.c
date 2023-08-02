9863f860b9a5b56b676ea9acc210c84fe24a6613
Cimorexave <sadeq.s502@gmail.com>
2023-08-03 00:17:01 +0330

    string manipulation

---
+++ str_man.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char[] str_clear(char[] * str)
+{
+    return *str = [];
+}
+
+void main()
+{
+    char my_str[10];
+    my_str = "hello there";
+
+    printf("");
+
+    return;
+}
