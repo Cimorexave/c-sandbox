e0403b6d772a989bddef988031464687055d6a62
Cimorexave <sadeq.s502@gmail.com>
2023-08-03 10:23:59 +0330

    fix: string length and pointer

--- str_man.c
+++ str_man.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int str_length(char *str)
 {
@@ -21,7 +22,8 @@ int main()
 
     char *my_str = "Hello There";
 
-    printf("the string \"%c\"'s length is: %i\n", *my_str, str_length(my_str));
+    printf("the string \"%s\"'s length is: %i\n", my_str, str_length(my_str));
+    printf("the string \"%s\"'s length is: %i\n", my_str, strlen(my_str)); // doesn't consider the 0 char at the end of the string
 
     return 0;
 }
