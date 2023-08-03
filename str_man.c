f17d45a4bc687f135a3a1ae01e8bed5e9a97ee15
Cimorexave <sadeq.s502@gmail.com>
2023-08-03 10:43:21 +0330

    copy string

--- str_man.c
+++ str_man.c
@@ -17,6 +17,19 @@ int str_length(char *str)
     return i;
 }
 
+void copy_str(char *str, char *copy)
+{
+    // copy[50];
+
+    int i = 0;
+    while (str[i] != 0)
+    {
+        copy[i] = str[i];
+        i++;
+    }
+    copy[i] = 0;
+}
+
 int main()
 {
 
@@ -24,6 +37,12 @@ int main()
 
     printf("the string \"%s\"'s length is: %i\n", my_str, str_length(my_str));
     printf("the string \"%s\"'s length is: %i\n", my_str, strlen(my_str)); // doesn't consider the 0 char at the end of the string
+
+    char *my_str2 = "sina";
+    char my_str2_copy[5];
+
+    copy_str(my_str2, my_str2_copy);
+    printf("copy of my_str2: %s is: %s\n", my_str2, my_str2_copy);
 
     return 0;
 }
