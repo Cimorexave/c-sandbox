88b15ae2e614dffcbfb2b57d513269338769b6af
Cimorexave <sadeq.s502@gmail.com>
2023-08-03 09:44:04 +0330

    array manipulations

---
+++ arr_man.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int length(int *arr)
+{
+    bool condition = true;
+    int i = 0;
+
+    while (condition)
+    {
+        if (arr[i] == 0)
+            condition = false;
+        i++;
+    }
+
+    return i;
+}
+
+int main()
+{
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    int *p = numbers;
+
+    printf("length of numbers: %i\n", length(numbers));
+
+    return 0;
+}
