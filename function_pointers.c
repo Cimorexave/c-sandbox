7eb1e58c7e20c96b7b394a3d295e4daaeea9218a
Cimorexave <sadeq.s502@gmail.com>
2025-09-24 14:01:57 +0200

    practicing with defining, typedef and assigining function pointers

--- function_pointers.c
+++ function_pointers.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
-int add_numbers(int a, int b);
+int add_numbers(int , int);
 void f1();
+int sqf (int);
+
+/* function pointers def */
+typedef int (*sqfptr) (int);
 
 int main() {
-    printf("HEY");
+    printf("INIT...\n");
     add_numbers(1,2);
     // void *f1p = &f1;
+    // void (*f1p)() = &f1;
     // printf("f1p: %p\n", f1p);
 
     // int *anp = &add_numbers(1,1);
-    // printf("anp: %p\n", anp);
+    int (*anp)(int,int) = &add_numbers;
+    printf("anp: %p\n", anp);
+
+    // sqfptr = &sqf;
+    // printf("sqfptr: %p\n", sqfptr);
+    
+    int (*sqfptr2) (int) = &sqf;
+    printf("sqfptr2: %p\n", sqfptr2);
+    
+    void  (*fptr)();
+    printf("fptr: %p\n", fptr);
+    fptr = &f1;
+    printf("fptr after assignment: %p\n", fptr);
+
     return 0;
 }
 
@@ -19,3 +37,6 @@ int add_numbers(int a, int b) {
     sum = a + b;
     return sum; // Returns the calculated sum
 }
+
+int sqf(int a) { return a ^ 2 ;}
+void f1(){return;}
