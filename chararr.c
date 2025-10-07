171d637487c428016732f1bafcf10c86ac6fe344
Cimorexave <sadeq.s502@gmail.com>
2025-10-07 12:19:22 +0200

    arg string output

---
+++ chararr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char* argv[]) {
+
+    printf("argv was: %s\n",argv[0]);
+    printf("argv was: %s\n",argv[1]);
+    printf("argv was: %s\n",argv[2]);
+    return 0;
+}
