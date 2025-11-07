b2da57d12c8b90e892a5e6099ff27c29a6bb4101
Sina Sadeghi <sadeq.s502@gmail.com>
2025-11-07 13:48:06 +0100

    swapping

--- refs.cpp
+++ refs.cpp
@@ -1,11 +1,26 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+
+void swap (int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    return;
+}
 
 int main() {
     int value1 = 5;
-    int& ref = value1;
-    std::cout << "ref:" << &ref << std::endl;
+    int& ref1 = value1;
     int value2 = value1;
     value2++;
-    std::cout << "ref:" << ref << std::endl;
+    int& ref2 = value2;
+    
+    std::cout << "value1, value2:" << value1 << ", " << value2 << std::endl;
+    std::cout << "value1 ad, value2 ad:" << &value1 << ", " << &value2 << std::endl;
+    swap(value1, value2);
+    std::cout << "value1, value2:" << value1 << ", " << value2 << std::endl;
+    std::cout << "value1 ad, value2 ad:" << &value1 << ", " << &value2 << std::endl;
+    
     return 0;
 }
+
