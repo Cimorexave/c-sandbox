2c4c2527153a9ca08e4d656f2ba78b2567f55d27
Sina Sadeghi <sadeq.s502@gmail.com>
2025-11-09 11:21:45 +0100

    class copy from init list

--- initializers.cpp
+++ initializers.cpp
@@ -15,6 +15,27 @@ int main() {
     //     std::cout << "number_Copy member: " << num << std::endl;
     // }
     
+      
+    std::cout << "init." << std::endl;
+    c1 instance_c1 = {1,2,3};
+    for (int i = 0; i < instance_c1.size(); i++) {
+        std::cout << "c1 at" << i << instance_c1.data()[i] << std::endl;
+    }
+
     return 0;
 }
 
+class c1 {
+    public:
+    int* _data;
+    size_t _size;
+    c1(std::initializer_list<int> numbers) : _size(numbers.size()) {
+        int i = 0;
+        _data = new int[numbers.size()];
+        for (const int& number : numbers) {
+            _data[i++] = number;
+        }
+    }
+    int* data() {return _data;}
+    int size() {return _size;}
+};
